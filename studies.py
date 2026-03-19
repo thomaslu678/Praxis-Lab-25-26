fd840cadee1e4a9dc9417290e6a24810ad05cb6b
JohnnyG256 <135672955+JohnnyG256@users.noreply.github.com>
2026-03-19 00:34:54 -0600

    Update data acquisition module to be functional

--- studies.py
+++ studies.py
@@ -1,23 +1,63 @@
-from typing import Literal, TypedDict, override
+from typing import TypedDict
 
 import ee
 import pandas as pd
-
-# Definitions
-project_name = "gee-481701"
-dataset_path = "LANDSAT/LC08/C02/T1_L2"
+import requests
 
 # Initial setup
+project_name = "gee-481701"
+dataset_path = "LANDSAT/LE07/C02/T1_L2"
 
 ee.Authenticate()
 ee.Initialize(project=project_name)
 
-landsat: ee.ImageCollection = ee.ImageCollection(dataset_path)
+# Combine Landsat collections
+ls9: ee.ImageCollection = ee.ImageCollection("LANDSAT/LC09/C02/T1_L2")
+ls9 = ls9.select([
+    "SR_B2", "SR_B3", "SR_B4", "SR_B5", "SR_B6", "SR_B7", "ST_B10", "QA_PIXEL", "QA_RADSAT"
+], [
+    "blue", "green", "red", "nir", "swir1", "swir2", "lst", "QA_PIXEL", "QA_RADSAT"
+])
+
+ls8: ee.ImageCollection = ee.ImageCollection("LANDSAT/LC08/C02/T1_L2")
+ls8 = ls8.select([
+    "SR_B2", "SR_B3", "SR_B4", "SR_B5", "SR_B6", "SR_B7", "ST_B10", "QA_PIXEL", "QA_RADSAT"
+], [
+    "blue", "green", "red", "nir", "swir1", "swir2", "lst", "QA_PIXEL", "QA_RADSAT"
+])
+
+ls7: ee.ImageCollection = ee.ImageCollection("LANDSAT/LE07/C02/T1_L2")
+ls7 = ls7.select([
+    "SR_B1", "SR_B2", "SR_B3", "SR_B4", "SR_B5", "SR_B7", "ST_B6", "QA_PIXEL", "QA_RADSAT"
+], [
+    "blue", "green", "red", "nir", "swir1", "swir2", "lst", "QA_PIXEL", "QA_RADSAT"
+])
+
+ls5: ee.ImageCollection = ee.ImageCollection("LANDSAT/LT05/C02/T1_L2")
+ls5 = ls5.select([
+    "SR_B1", "SR_B2", "SR_B3", "SR_B4", "SR_B5", "SR_B7", "ST_B6", "QA_PIXEL", "QA_RADSAT"
+], [
+    "blue", "green", "red", "nir", "swir1", "swir2", "lst", "QA_PIXEL", "QA_RADSAT"
+])
+
+ls4: ee.ImageCollection = ee.ImageCollection("LANDSAT/LT04/C02/T1_L2")
+ls4 = ls4.select([
+    "SR_B1", "SR_B2", "SR_B3", "SR_B4", "SR_B5", "SR_B7", "ST_B6", "QA_PIXEL", "QA_RADSAT"
+], [
+    "blue", "green", "red", "nir", "swir1", "swir2", "lst", "QA_PIXEL", "QA_RADSAT"
+])
+
+landsat: ee.ImageCollection = ls9.merge(ls8).merge(ls7).merge(ls5).merge(ls4)
+
+# Only include data from June, July, and August
+landsat = landsat.filter(
+    ee.Filter.calendarRange(6, 8, "month")
+)
 
 # Scale Landsat data to useful measurements
 def _scale_landsat(image: ee.Image) -> ee.Image:
-    optical_bands = image.select('SR_B.').multiply(0.0000275).add(-0.2)
-    thermal_bands = image.select('ST_B.*').multiply(0.00341802).add(149.0)
+    optical_bands = image.select("blue", "green", "red", "nir", "swir1", "swir2").multiply(0.0000275).add(-0.2)
+    thermal_bands = image.select("lst").multiply(0.00341802).add(149.0)
 
     return image.addBands(optical_bands, None, True).addBands(
         thermal_bands,
@@ -27,26 +67,31 @@ def _scale_landsat(image: ee.Image) -> ee.Image:
 
 landsat = landsat.map(_scale_landsat)
 
+
+# Add extra bands: time, ndvi
+landsat = landsat.map(
+    lambda image: image.addBands(
+        ee.Image.constant(image.get("system:time_start")).rename("time")
+    ).addBands(
+        image.normalizedDifference(["nir", "red"]).rename("ndvi")
+    )
+)
+
 # Mask pixels with clouds or water, or saturated pixels
 def _apply_mask(image: ee.Image) -> ee.Image:
-    # Bit 0: Unused
+    # Bit 0: Fill
     # Bit 1: Dilated Cloud
     # Bit 2: Cirrus (high confidence)
     # Bit 3: Cloud
     # Bit 4: Cloud Shadow
     # Bit 7: Water
-    qa_mask = image.select("QA_PIXEL").bitwiseAnd(int("10011111", 2)).Not()
-    saturation_mask = image.select("QA_RADSAT").Not()
+    qa_mask = image.select("QA_PIXEL").bitwiseAnd(int("10011111", 2)).eq(0)
+
+    saturation_mask = image.select("QA_RADSAT").eq(0)
 
     return image.updateMask(qa_mask).updateMask(saturation_mask)
 
 landsat = landsat.map(_apply_mask)
-
-# Only include data from June, July, and August
-landsat = landsat.filter(
-    ee.Filter.calendarRange(6, 8, "month")
-)
-
 
 class CaseStudy(TypedDict):
     """Collection of information for a singular case study."""
@@ -59,13 +104,10 @@ class CaseStudy(TypedDict):
 
     end: int
     """Year of construction concluding."""
-
-    outline: ee.Geometry.Polygon
-    """Bounds of case study area."""
-
-    points: ee.FeatureCollection
-    """Collection of transect samples for case study.
     
+    points: ee.FeatureCollection
+    """URL to GeoJSON file containing samples points of study.
+
     Each feature should be an ee.Point with a distance property.
     """
 
@@ -73,79 +115,75 @@ class CaseStudy(TypedDict):
     """WMO URL used to obtain local meterological station data."""
 
 
-# Primary function used to interact with this module
-def getData(study: CaseStudy) -> pd.DataFrame:
-    """Obtain relevant Landsat data samples for a specified case study.
+def _generateData(study: CaseStudy):
+    """Generate relevant Landsat data samples for a specified case study.
 
-    The data is returned as a pandas DataFrame with the following columns:
+    The data is returned as a feature collection. Each feature has the following properties:
         study_name
         similarity
         distance
         time
-        lst
-        ndvi
         station_temp
         station_humidity
-    
-    study_name and similarity will be identical for all rows of a singular case study.
+        blue
+        green
+        red
+        nir
+        swir1
+        swir2
+        lst
+
+    study_name and similarity will be identical for all data for a singular case study.
 
     Args:
-        case_study: The case study to obtain data for.
-    Returns: The data for the case study.
+        case_study:
+            The case study to obtain data for.
+    Returns:
+        A feature collection. Each feature represents a singular sample in time and space.
     """
 
     # Validate parameters
 
     # TODO: Add validation for similarity once similarity score defined
-
     if (study["start"] > study["end"]):
         raise ValueError(f"{study["name"]}: End year should be after start year")
-    
+
     if (not study["points"].propertyNames().contains("distance")):
         raise ValueError(f"{study["name"]}: Collection of points should have \"distance\" property.")
-    
+
     try:
         study["points"].map(lambda point: ee.Geometry.Point(point.geometry()))
     except:
         raise ValueError(f"{study["name"]}: Points should be convertable to ee.Geometry.Point instances")
-    
-    if (study["points"].filter(
-        lambda point: point.get("distance") 
-    ) > 0):
-        raise ValueError("All point distances should be non-negative")
-    
+
     # Include data one year before and 5 years after implementation
     start = study["start"] - 1
     end = study["end"] + 6 # End bound is exclusive
 
-    dataset: ee.FeatureCollection = landsat.filterDate(str(start), str(end))
-    
-    # Map each point to a feature collection containing Landsat data
-    def map_point(feature: ee.Feature) -> ee.FeatureCollection:
+    images: ee.ImageCollection = landsat.filterDate(str(start), str(end))
 
-        point = ee.Geometry.Point(feature.geometry())
+    # Only include images that intersect at least one sample point
+    images = images.filterBounds(study["points"].bounds())
 
-        # Only include Landsat images that intersect point
-        point_data = dataset.filterBounds(point)
-
-        # Map each image to a feature with relevant data and no geometry
-        point_data.map(
-            lambda image: ee.Feature(
-                None, {
-                    "study_name": study["name"],
-                    "similarity": study["similarity"],
-                    "time": image.get("DATE_PRODUCT_GENERATED"),
-                    "distance": feature.get("distance"),
-                    "lst": image.select("ST_B10"),
-                    "ndvi": image.select("NDVI"),
-                    # TODO: look into fetching station data
-                    "station_temp": None,
-                    "station_humidity": None
-                }
-            )
+    # For each image, take samples at all intersecting points
+    samples = images.map(
+        lambda image: image.reduceRegions(
+            collection = study["points"],
+            reducer = ee.Reducer.first(),
+            scale = 30, # meters
         )
-    
-    # Convert collection of collections to DataFrame
-    return pd.DataFrame(
-        study["points"].map(map_point).flatten()
     )
+
+    # Convert FeatureCollection to DataFrame
+    samples = samples.flatten()
+
+    # Remove samples that didn't have an unmasked pixel intersecting point
+    samples = samples.filter(ee.Filter.neq("time", None))
+
+    data = samples.first().propertyNames().iterate(
+        lambda curr, prev: ee.Dictionary(prev).set(curr, samples.aggregate_array(curr)),
+        ee.Dictionary()
+    )
+
+    # Return samples
+    return samples
